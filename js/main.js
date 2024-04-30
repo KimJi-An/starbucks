eef404c0942ee32cbe935dbe944deb21e7684a94
KimJi-An <j0012a@naver.com>
2024-04-30 19:26:17 +0900

    슬라이드 영역 토글

--- js/main.js
+++ js/main.js
@@ -82,3 +82,19 @@ new Swiper('.promotion .swiper-container', {
     nextEl: '.promotion .swiper-next'
   }
 });
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;  // 반대 값 반환
+
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리
+    promotionEl.classList.remove('hide');
+  }
+});
