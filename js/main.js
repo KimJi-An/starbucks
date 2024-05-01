52332747b7f5e24ab596d1b42ef576c710a23e66
KimJi-An <j0012a@naver.com>
2024-05-01 18:18:33 +0900

    반복 애니메이션

--- js/main.js
+++ js/main.js
@@ -98,3 +98,28 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionEl.classList.remove('hide');
   }
 });
+
+function random(min, max) {
+  // .toFixed()를 통해 반환된 문자 데이터를
+  // parseFloat()를 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector,  // 선택자
+    random(1.5, 2.5),  // 애니메이션 동작 시간
+    {  // 옵션
+      y: size,
+      repeat: -1, // -1 : 무한 반복
+      yoyo: true,
+      ease: Power1.easeInOut,  // 반복을 자연스럽게
+      delay: random(0, delay),
+    }
+  );
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
